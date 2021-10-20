f75d01d043964c62be4d6ff71368d9abb8562a51
Pham Hong Thai <hongthaipro@gmail.com>
2021-10-20 13:50:06 +0700

    bo helmet

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import * as helmet from 'helmet';
+
 import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  await app.use(helmet({ contentSecurityPolicy: false }));
   const config = new DocumentBuilder()
     .setTitle('Pay Gate API')
     .setDescription('The API for crypto pay gate')
